faab9c7e37268b42ad098424c5c5e9ef2a270776
Matt Eland <Matt.Eland@GMail.com>
2024-06-12 23:01:37 -0400

    Added a pair of column methods

---
+++ MattEland.ML/MattEland.ML.DataFrames/DataFrameColumnCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.Analysis;
+
+namespace MattEland.ML.DataFrames;
+
+public static class DataFrameColumnCollectionExtensions
+{
+    /// <summary>
+    /// Removes one or more columns from a DataFrame and returns the count removed.
+    /// </summary>
+    /// <param name="columns">The collection of columns</param>
+    /// <param name="columnsToRemove">The columns to remove</param>
+    /// <returns>The number of columns actually removed.</returns>
+    public static int Remove(this DataFrameColumnCollection columns, params string[] columnsToRemove)
+    {
+        int removed = 0;
+        foreach (var column in columnsToRemove)
+        {
+            var match = columns.FirstOrDefault(c => c.Name == column);
+            if (match != null)
+            {
+                columns.Remove(match);
+                removed++;
+            }
+        }
+
+        return removed;
+    }
+    
+    /// <summary>
+    /// Removes all columns a DataFrame except those explicitly named as ones to keep and returns the count removed.
+    /// </summary>
+    /// <param name="columns">The collection of columns</param>
+    /// <param name="columnsToKeep">The columns to keep</param>
+    /// <returns>The number of columns actually removed.</returns>
+    public static int RemoveAllBut(this DataFrameColumnCollection columns, params string[] columnsToKeep)
+    {
+        int removed = 0;
+        DataFrameColumn[] existingColumns = columns.ToArray();
+        foreach (var column in existingColumns.Where(c => !columnsToKeep.Contains(c.Name)))
+        {
+            columns.Remove(column);
+            removed++;
+        }
+
+        return removed;
+    }    
+}
--- MattEland.ML/MattEland.ML.DataFrames/README.md
+++ MattEland.ML/MattEland.ML.DataFrames/README.md
@@ -3,3 +3,27 @@
 This library offers extensions to make working with the `Microsoft.Data.Analysis.DataFrame` class easier for data wrangling and machine learning workloads.
 
 This code is in early prototyping and the library is not yet meant to be broadly consumed.
+
+## Capabilities
+
+The library currently offers the following capabilities:
+
+### Removing multiple columns from a DataFrame
+
+Previously in order to remove multiple columns from a DataFrame you needed to create an enumerable and loop over it, removing each column in turn.
+
+This library provides a `myDataFrame.Columns.Remove` extension method that takes in a params array of column names to remove.
+
+Usage:
+```cs
+int columnsRemoved = myDataFrame.Columns.Remove("ColumnA", "ColumnB", "ColumnC");
+```
+
+Additionally, you can remove all columns except for a few specified columns with the `myDataFrame.Columns.RemoveAllBut` method.
+
+This is particularly helpful when you want drop all columns not needed for model training.
+
+Usage:
+```cs
+int columnsRemoved = myDataFrame.Columns.RemoveAllBut("ColumnC", "ColumnD");
+```
